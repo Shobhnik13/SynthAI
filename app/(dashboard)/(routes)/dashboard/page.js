326246617f5f23c896f204209edf7a6bbbfffa88
Shobhnik13 <shobhnikw@gmail.com>
2023-08-09 03:01:49 +0530

    initial

---
+++ app/(dashboard)/(routes)/dashboard/page.js
@@ -0,0 +1,8 @@
+
+const page = () => {
+  return (
+    <div>dashboard</div>
+  )
+}
+
+export default page
---
+++ app/(landing)/page.js
@@ -0,0 +1,8 @@
+
+const page = () => {
+  return (
+    <div>Hiiiiiii</div>
+  )
+}
+
+export default page
