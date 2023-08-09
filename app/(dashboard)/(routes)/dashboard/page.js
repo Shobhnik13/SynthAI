ebf175b5e4150d0092cfade5ee93f58e430baa9d
Shobhnik13 <shobhnikw@gmail.com>
2023-08-10 00:21:05 +0530

    setup sign-in and sign-up using clerk auth

--- app/(dashboard)/(routes)/dashboard/page.js
+++ app/(dashboard)/(routes)/dashboard/page.js
@@ -1,7 +1,10 @@
+import { UserButton } from "@clerk/nextjs"
 
 const page = () => {
   return (
-    <div>dashboard</div>
+    <div>dashboard
+      <UserButton afterSignOutUrl="/"/>
+    </div>
   )
 }
 
--- app/(landing)/page.js
+++ app/(landing)/page.js
@@ -1,7 +1,18 @@
-
+import Link from "next/link"
+import {Button} from '@/components/ui/button'
 const page = () => {
   return (
-    <div>Hiiiiii</div>
+    <div>
+      landing
+      <div>
+        <Link href={'/sign-in'}>
+        <Button>Login</Button>
+        </Link>
+        <Link href={'/sign-up'}>
+        <Button>Register</Button>
+        </Link>
+      </div>
+    </div>
   )
 }
 
