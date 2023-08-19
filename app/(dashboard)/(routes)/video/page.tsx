90a92eae1e8602c30bc08925ab309dba74dc8e6f
Shobhnik13 <shobhnikw@gmail.com>
2023-08-19 13:09:36 +0530

    video gen done

---
+++ app/(dashboard)/(routes)/video/page.tsx
@@ -0,0 +1,98 @@
+'use client'
+import { Loader, MessageSquare, Music2Icon, MusicIcon, VideoIcon } from "lucide-react"
+import { Form, FormControl, FormField, FormItem, FormLabel } from "../../../../components/ui/form"
+import Heading from "../../../../components/heading"
+import { useForm } from 'react-hook-form'
+import * as z from "zod"
+import { formSchema } from "./video"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { Input } from "../../../../components/ui/input"
+import axios from 'axios'
+import { Button } from "../../../../components/ui/button"
+import { useRouter } from "next/navigation"
+import { useState } from "react"
+import { ChatCompletionRequestMessage } from "openai"
+import Empty from "../../../../components/ui/empty"
+import LoaderComp from "../../../../components/ui/loadercomp"
+import { cn } from "../../../../lib/utils"
+import UserAva from "../../../../components/ui/user-ava"
+import BotAva from "../../../../components/ui/bot-ava"
+
+const MusicPage = () => {
+ 
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      prompt: "",
+    },
+  })
+
+  const isLoading=form.formState.isSubmitting
+  const router=useRouter()
+  // video -> Its a string(link) which is currently undefined/empty
+  const [video,setVideo]=useState<string>()
+  const onSubmit=async(values: z.infer<typeof formSchema>)=>{
+    try{
+        setVideo(undefined)
+      //api call
+      const res=await axios.post('/api/video',values)
+      setVideo(res.data[0])
+      form.reset()
+    }catch(error: any){
+        console.log(error)
+    }finally{
+        router.refresh()
+    }
+  }
+  return (
+    <div>
+        <Heading
+        title='Video'
+        description='Our most advanced video generation model'
+        icon={VideoIcon}
+        iconColor='text-orange-700'
+        bgColor='bg-orange-700/10'/>
+        
+        {/* form  */}
+
+        {/* main div ->form + message content */}
+        <div className="px-4 lg:px-8">
+          {/* form div  */}
+          <div>
+            <Form {...form}>
+              <form onSubmit={form.handleSubmit(onSubmit)} className="rounded-lg border w-full p-4 px-3 md:px-6 focus-within:shadow-sm grid grid-cols-12 gap-2">
+                <FormField
+                control={form.control}
+                name="prompt"
+                render={({ field }) => (
+                  <FormItem className="col-span-12 lg:col-span-10">
+                        <FormControl >
+                          <Input placeholder="Convert your prompt into video" disabled={isLoading} className="border-0 outline-none focus-visible:ring-0 focus-visible:ring-transparent" {...field} />
+                        </FormControl>
+                  </FormItem>
+                )}/>
+                <Button type="submit" className="col-span-12 md:col-span-2 w-full" disabled={isLoading}>{isLoading?'Generating...':'Generate'}</Button>
+              </form>
+            </Form>
+          </div>
+          <div className="space-y-4 mt-4 ">
+            {isLoading && (
+              <div className="p-8 rounded-lg w-full flex items-center justify-center bg-muted">
+                  <LoaderComp purpose='video' instruction='It may take upto 3-5 minutes!'/>
+              </div>
+            )}
+            {!video && !isLoading && (
+              <div><Empty label="No music started yet!"/></div>
+            )}
+           <div>
+                {video && <video controls className="w-full mt-8 aspect-video rounded-lg border bg-black">
+                    <source src={video}/>
+                </video>}
+           </div>
+          </div>
+        </div>
+    </div>
+  )
+}
+
+export default MusicPage
--- app/api/video/route.ts
+++ app/api/video/route.ts
@@ -22,16 +22,16 @@ export async function POST(req:Request){
             }
             // returning response 
             const res = await replicate.run(
-                "riffusion/riffusion:8cf61ea6c56afd61d8f5b9ffd14d7c216c0a93844ce2d82ac1c9ecc9c7f24e05",
+                "anotherjesse/zeroscope-v2-xl:9f747673945c62801b13b84701c783929c0ee784e4748ec062204894dda1a351",
                 {
                   input: {
-                    prompt_a: prompt
+                    prompt: prompt
                   }
                 }
               );
             return NextResponse.json(res)
     }catch(error){
-        console.log('Music error!',error)
+        console.log('Video error!',error)
         return new NextResponse('Internal error',{status:500})
     }
 }
